0135363b07772ed24fb879ddc0f5daf3b6d75458
samtyblaze <oladapotayo77@gmail.com>
2023-08-13 17:31:10 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
- * Main - A c program that prints a line to the standard error
- * Return: 0 (success)
+ * main - Entry point
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	return (1);
 }
