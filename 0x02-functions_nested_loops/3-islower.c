7d740576dbbd4a436b11e72e0ddbd476c044204a
samtyblaze <oladapotayo77@gmail.com>
2023-08-16 05:26:06 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,15 +6,14 @@
  * Return: 1 for lowercase character or 0 for anything else
  */
 
-
 int _islower(int c)
 
 {
 
-	if(c >=97 && c<=122)
+	if (c >= 97 && c <= 122)
 	{
 
-	return(1);
+	return (1);
 	}
 	return (0);
 }
